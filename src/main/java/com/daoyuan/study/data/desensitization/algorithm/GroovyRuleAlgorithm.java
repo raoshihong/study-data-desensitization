2d231d5f9ead99633b1e52232c8508afa0c65b69
honey.rao <honey.rao@wolaidai.com>
2019-11-29 18:06:07 +0800

    提交算法

---
+++ src/main/java/com/daoyuan/study/data/desensitization/algorithm/GroovyRuleAlgorithm.java
@@ -0,0 +1,10 @@
+package com.daoyuan.study.data.desensitization.algorithm;
+
+import com.daoyuan.study.data.desensitization.domain.Algorithm;
+
+public class GroovyRuleAlgorithm implements RuleAlgorithm {
+    @Override
+    public String algorithm(Algorithm algorithm, String source) {
+        return null;
+    }
+}
